4fc0eb54f694c87a95cf26b6fa6113a762314766
akash-verma22 <akashvverma01@gmail.com>
2025-07-12 01:53:19 +0530

    Add Sanity Schemas

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({searchParams}:{
   searchParams: Promise<{query?: string}>
   }) {
 
   const query = (await searchParams).query;
+
+  const posts = await client.fetch(STARTUPS_QUERY);
+
+
 
   return (
     <>
@@ -21,6 +28,22 @@ export default async function Home({searchParams}:{
 
       </section>
       
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"`: `All Startups`}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length>0?(
+            posts.map((post:StartupCardType,index:number)=>(
+              <StartupCard key={post?._id} post={post}/>
+            ))
+          ):(
+            <p className="no-results">No Startups Found!</p>
+          )}
+        </ul>
+      </section>
+
+
     </>
   );
 }
